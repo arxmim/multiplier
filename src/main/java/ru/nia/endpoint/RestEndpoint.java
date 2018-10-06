0183b98e6717fe6538545fcdd02b6c175cd5d34c
ivannaz@yandex.ru <ksusha21>
2018-10-06 23:13:56 +0300

    add html + part of degree (minutes)

--- src/main/java/ru/nia/endpoint/RestEndpoint.java
+++ src/main/java/ru/nia/endpoint/RestEndpoint.java
@@ -1,15 +1,18 @@
 package ru.nia.endpoint;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apfloat.Apfloat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.nia.service.MultiplierService;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class RestEndpoint {
 
     private MultiplierService service;
@@ -19,10 +22,11 @@ public class RestEndpoint {
     }
 
     @RequestMapping("/doRecurCos")
-    public List<String> doRecurCos(@RequestParam(value = "step") int step_size, int step_count) {
+    public List<String> doRecurCos(@RequestParam(value = "step") BigDecimal step_size, int step_count) {
+        log.info("invoke COS for params: step_size=" + step_size + ", step_count=" + step_count);
         ArrayList<String> strings = new ArrayList<>();
         Apfloat first = service.getCos(step_size);
-        Apfloat second = service.getCos(step_size * 2);
+        Apfloat second = service.getCos(step_size.multiply(new BigDecimal(2)));
 
         strings.add(first.toString(true));
         strings.add(second.toString(true));
@@ -36,10 +40,11 @@ public class RestEndpoint {
     }
 
     @RequestMapping("/doRecurSin")
-    public List<String> doRecurSin(@RequestParam(value = "step") int step_size, int step_count) {
+    public List<String> doRecurSin(@RequestParam(value = "step") BigDecimal step_size, int step_count) {
+        log.info("invoke SIN for params: step_size=" + step_size + ", step_count=" + step_count);
         ArrayList<String> strings = new ArrayList<>();
         Apfloat first = service.getSin(step_size);
-        Apfloat second = service.getSin(step_size * 2);
+        Apfloat second = service.getSin(step_size.multiply(new BigDecimal(2)));
 
         strings.add(first.toString(true));
         strings.add(second.toString(true));
--- src/main/java/ru/nia/service/MultiplierService.java
+++ src/main/java/ru/nia/service/MultiplierService.java
@@ -3,23 +3,25 @@ package ru.nia.service;
 import org.apfloat.Apfloat;
 import org.apfloat.ApfloatMath;
 
+import java.math.BigDecimal;
+
 public class MultiplierService {
     private static final int precision = 80;
 
-    public Apfloat getCos(int val) {
+    public Apfloat getCos(BigDecimal val) {
         return ApfloatMath.cos(ApfloatMath.toRadians(new Apfloat(val, precision))).precision(precision);
     }
 
-    public Apfloat getSin(int val) {
+    public Apfloat getSin(BigDecimal val) {
         return ApfloatMath.sin(ApfloatMath.toRadians(new Apfloat(val, precision))).precision(precision);
     }
 
-    private Apfloat getK(int step) {
+    private Apfloat getK(BigDecimal step) {
         Apfloat apfloat = getCos(step);
         return apfloat.multiply(new Apfloat(2, precision));
     }
 
-    public Apfloat doRecur(Apfloat second, Apfloat first, int step) {
+    public Apfloat doRecur(Apfloat second, Apfloat first, BigDecimal step) {
         Apfloat res = getK(step);
 
         res = res.multiply(second).precision(precision);
